58ab9bd42d2b88a7cce7c1b2051c57fc1abe0476
smahad09 <mahadsyed8@gmail.com>
2022-11-14 23:04:01 +0500

    Add project files.

---
+++ MM-Perfumes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MM_Perfumes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
